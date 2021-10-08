2f73da9ae6c8f43cf5a49be24788d65505ae4ead
franciscojesus.lara <franciscojesus.lara@petroprix.com>
2021-10-08 13:48:19 +0200

    Segundo commit

--- pythonflask/app.py
+++ pythonflask/app.py
@@ -1,20 +1,37 @@
 import bcrypt
 from flask import json
 from forms import SignupForm,SigninForm
-from flask import Flask, request, abort,render_template,redirect,url_for
+from flask import Flask, request,session, abort,render_template,redirect,url_for
 from flask.json import jsonify
+from flask_session import Session
 from flask_bcrypt import Bcrypt
 from config import ApplicationConfig
 from models import db,User
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt=Bcrypt(app)
+cors = CORS(app, supports_credentials=True)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route("/@me")
+def get_curret_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error" : "Unauthorized"}) ,401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 @app.route("/register", methods=['GET','POST'])
 def register_user():
@@ -36,21 +53,23 @@ def register_user():
 
 @app.route("/login", methods=['GET','POST'])
 def login_user():
-    form = SigninForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-                          
-        user = User.query.filter_by(email=email).first()
+    email = request.json["email"]
+    password = request.json["password"]
 
-        if user is None:
-            return jsonify({"error" : "Unauthorized"}) ,401
-        
-        if not bcrypt.check_password_hash(user.password,password):
-            return jsonify({"error" : "Unauthorized"}) ,401
-        
-        return redirect(url_for('index'))
-    return render_template("signup_form.html", form=form)
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    session["user_id"] = user.id
+
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
 
 
 @app.route("/", methods=['GET'])
--- pythonflask/config.py
+++ pythonflask/config.py
@@ -1,5 +1,7 @@
 #from dotenv import load_dotenv
+from operator import truediv
 import os
+import redis
 
 #load_dotenv()
 SECRET_KEY="psgkjasdpgkajsdpgasdkjg"
@@ -8,3 +10,8 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
+
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
