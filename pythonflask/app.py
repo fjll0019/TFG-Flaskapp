eeef2cf264a65ec87d745f2543710769469977dc
Francisco Jesús Lara López <fjll0019@red.ujaen.es>
2022-05-30 19:11:18 +0200

    TFG V1.02

--- pythonflask/app.py
+++ pythonflask/app.py
@@ -12,6 +12,11 @@ from werkzeug.utils import secure_filename
 import os
 from elasticsearch import Elasticsearch
 import csv
+import random
+import string
+import smtplib,ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
@@ -20,6 +25,12 @@ UPLOAD_FOLDER2 = 'static/data'
 
 ALLOWED_EXTENSIONS = {'csv'}
 #es= Elasticsearch()
+
+smtp_address = 'smtp.gmail.com'
+smtp_port = 465
+
+email_address = 'enerhomeTFG@gmail.com'
+email_password = '#enerhomeTFG1'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['UPLOAD_FOLDER2'] = UPLOAD_FOLDER2
@@ -59,66 +70,78 @@ class energyData:
 @app.route("/@me", methods=['GET'])
 def get_curret_user():
     global allDates
+
     user_id = session.get("user_id")
-    user = User.query.filter_by(id=user_id).first()
-   # new_csv()
-    if user==None:
-        return jsonify({"error" : "Unauthorized"}) ,401
+    if user_id != None:
+        user = User.query.filter_by(id=user_id).first()
+    # new_csv()
+        if user==None:
+            return jsonify({"error" : "Unauthorized"}) ,401
+        
+        user = User.query.filter_by(id=user_id).first()
+        datas=[]
+        datos = Datos.query.filter_by(owner_id=user.id).all()
+        for data in datos:
+            datas.append(data.name)
+        es= Elasticsearch(['localhost:9200'])
+        body={"size":100}
+
+        resp=es.indices.get('*').keys()
+        sorted_list=sorted(resp)
+        test_str='.'
+        indices = []
+        fechas=[]
+        energia=[]
+        activePower=[]
+        voltage=[]
+        powerFactor=[]
+        apparentPower=[]
+        current=[]
+        for sub in sorted_list:
+            flag = 0
+            for ele in sub:
+                if ele in test_str:
+                    flag = 1
+            if not flag:
+                indices.append(sub)  
+
+        for index in indices:
     
-    user = User.query.filter_by(id=user_id).first()
-    datas=[]
-    datos = Datos.query.filter_by(owner_id=user.id).all()
-    for data in datos:
-        datas.append(data.name)
-    es= Elasticsearch(['localhost:9200'])
-    body={"size":100}
+            resp=es.search(index=index,body=body)   
+            data2 = [doc2 for doc2 in resp['hits']['hits']]
+        # print(data2)
+            cont=0
+        # print(index)
+            for doc2 in data2:
+                #print(doc2)
+                if doc2['_source'] != {}:
+                    horaaux=doc2['_source']["Hora"]
+                    fechaaux2=doc2['_source']["Fecha"]
+                    fechaaux=fechaaux2+":"+ str(horaaux)
+                #print(energia)
 
-    resp=es.indices.get('*').keys()
-    sorted_list=sorted(resp)
-    test_str='.'
-    indices = []
-    allEnergyData = []
-    fechas=[]
-    energia=[]
-    activePower=[]
-    voltage=[]
-    powerFactor=[]
-    apparentPower=[]
-    current=[]
-    for sub in sorted_list:
-        flag = 0
-        for ele in sub:
-            if ele in test_str:
-                flag = 1
-        if not flag:
-            indices.append(sub)  
-
-    for index in indices:
-   
-        resp=es.search(index=index,body=body)   
-        data2 = [doc2 for doc2 in resp['hits']['hits']]
-        #print(data2)
-        cont=0
-        print(index)
-        for doc2 in data2:
-        #   print(doc2)
-            fechaaux=doc2['_source']["Fecha"]
-            #print(energia)
-
-            if fechaaux not in fechas:
-                if len(fechas)>0:
-                    date1 = datetime.strptime(fechaaux, "%d/%m/%Y:%H")
-                    date2 = datetime.strptime(fechas[cont], "%d/%m/%Y:%H")
-                    print(date1<date2)
-                    if date1 < date2:
-                        fechas.insert(cont,fechaaux)
-                        energia.insert(cont,doc2['_source']["Energia"])
-                        activePower.insert(cont,doc2['_source']["P_Activa"])
-                        voltage.insert(cont,doc2['_source']["Tension"])
-                        powerFactor.insert(cont,doc2['_source']["F_Potencia"])
-                        apparentPower.insert(cont,doc2['_source']["P_Aparente"])
-                        current.insert(cont,doc2['_source']["Intensidad"])
-                        cont=cont+1
+                if fechaaux not in fechas:
+                    if len(fechas)>0:
+                        date1 = datetime.strptime(fechaaux, "%d/%m/%Y:%H")
+                        date2 = datetime.strptime(fechas[cont], "%d/%m/%Y:%H")
+                    # print(date1<date2)
+                        if date1 < date2:
+                            fechas.insert(cont,fechaaux)
+                            energia.insert(cont,doc2['_source']["Energia"])
+                            activePower.insert(cont,doc2['_source']["P_Activa"])
+                            voltage.insert(cont,doc2['_source']["Tension"])
+                            powerFactor.insert(cont,doc2['_source']["F_Potencia"])
+                            apparentPower.insert(cont,doc2['_source']["P_Aparente"])
+                            current.insert(cont,doc2['_source']["Intensidad"])
+                            cont=cont+1
+                        else:
+                            fechas.append(fechaaux)
+                            energia.append(doc2['_source']["Energia"])
+                            activePower.append(doc2['_source']["P_Activa"])
+                            voltage.append(doc2['_source']["Tension"])
+                            powerFactor.append(doc2['_source']["F_Potencia"])
+                            apparentPower.append(doc2['_source']["P_Aparente"])
+                            current.append(doc2['_source']["Intensidad"])
                     else:
                         fechas.append(fechaaux)
                         energia.append(doc2['_source']["Energia"])
@@ -128,49 +151,49 @@ def get_curret_user():
                         apparentPower.append(doc2['_source']["P_Aparente"])
                         current.append(doc2['_source']["Intensidad"])
                 else:
-                    fechas.append(fechaaux)
-                    energia.append(doc2['_source']["Energia"])
-                    activePower.append(doc2['_source']["P_Activa"])
-                    voltage.append(doc2['_source']["Tension"])
-                    powerFactor.append(doc2['_source']["F_Potencia"])
-                    apparentPower.append(doc2['_source']["P_Aparente"])
-                    current.append(doc2['_source']["Intensidad"])
-            else:
-                i=0
-                while len(fechas):
-                    if fechas[i] == fechaaux:
-                        #print(fechaaux)
-                        energia[i]=(energia[i]+(doc2['_source']["Energia"]))/2
-                        activePower[i]=(activePower[i]+doc2['_source']["P_Activa"])/2
-                        voltage[i]=(voltage[i]+doc2['_source']["Tension"])/2
-                        powerFactor[i]=(powerFactor[i]+doc2['_source']["F_Potencia"])/2
-                        apparentPower[i]=(apparentPower[i]+doc2['_source']["P_Aparente"])/2
-                        current[i]=(current[i]+doc2['_source']["Intensidad"])/2
+                    i=0
+                #  print(len(fechas))
+                    while i< len(fechas):
+                        #print(i)
+                        if fechas[i] == fechaaux:
+                            #print(fechaaux)
+                            if doc2['_source'] != {}:
+                                energia[i]=(energia[i]+(doc2['_source']["Energia"]))/2
+                                activePower[i]=(activePower[i]+doc2['_source']["P_Activa"])/2
+                                voltage[i]=(voltage[i]+doc2['_source']["Tension"])/2
+                                powerFactor[i]=(powerFactor[i]+doc2['_source']["F_Potencia"])/2
+                                apparentPower[i]=(apparentPower[i]+doc2['_source']["P_Aparente"])/2
+                                current[i]=(current[i]+doc2['_source']["Intensidad"])/2
+                                
+                                break
+                        i+=1    
+            #  reactivePower.append(doc['_source']["P_Aparente(VA)"])
 
-                        break
-                    i+=1
-        #  reactivePower.append(doc['_source']["P_Aparente(VA)"])
+        fechas.sort(key=lambda date: datetime.strptime(date, "%d/%m/%Y:%H"))
+        allDates=fechas
+        indices2=[]
+        for fichero in datas :
+            aux=fichero.split(".", 1)
+            indices2.append(aux[0].lower())
 
-    fechas.sort(key=lambda date: datetime.strptime(date, "%d/%m/%Y:%H"))
-    allDates=fechas
-    return jsonify({
-        "id": user.id,
-        "email": user.email,
-        "nombre" : user.nombre,
-        "avatar": user.avatar,
-        "data": datas,
-        "rol" : user.rol , 
-        "indices": indices,
-        "mediaCurrent":current,
-        "mediaEnergia":energia,
-        "mediaActivePower":activePower,
-        "mediaVoltage":voltage,
-        "mediaPowerFactor":powerFactor,
-        "mediaApparentPower":apparentPower,
-        "mediaCurrent":current,
-        "labels": fechas
-    })
-
+        return jsonify({
+            "id": user.id,
+            "email": user.email,
+            "nombre" : user.nombre,
+            "avatar": user.avatar,
+            "data": datas,
+            "rol" : user.rol , 
+            "indices": indices2,
+            "mediaCurrent":current,
+            "mediaEnergia":energia,
+            "mediaActivePower":activePower,
+            "mediaVoltage":voltage,
+            "mediaPowerFactor":powerFactor,
+            "mediaApparentPower":apparentPower,
+            "mediaCurrent":current,
+            "labels": fechas
+        })
+    return jsonify({}), 200
 @app.route("/getData", methods=['GET'])
 def get_data():
     startDate = request.args['startDate']
@@ -181,9 +204,11 @@ def get_data():
     es= Elasticsearch(['localhost:9200'])
     lista = []
     fechas=[]
+    #print(allDates)
+
     for index in files:
-        
-        body={"size":100}
+        #
+        body={"size":100,"query": {"bool":{"must":[{"range": {"Fecha": {"gte": startDate,"lte": finishDate}}}]}}}
         resp=es.search(index=index,body=body,scroll='5s')  
         newSize=len(allDates)
         energia=[0] * newSize
@@ -196,20 +221,23 @@ def get_data():
         cont=0
         parsedDates=False
         isDateGreater=False
-        #print(resp['hits']['hits'])
+       # print(resp['hits']['hits'])
         data = [doc for doc in resp['hits']['hits']]    
         for doc in data:
             #print(doc)
             #fechaaux2=fechaaux.split(".", 1)
             #if fechaaux2[0] in fechas:
-            fecha=doc['_source']["Fecha"]
+            if doc['_source'] != {}:
+                horaaux=doc['_source']["Hora"]
+                fechaaux2=doc['_source']["Fecha"]
+                fecha=fechaaux2+":"+ str(horaaux)
             i=0
             if parsedDates==False:
                 date1 = datetime.strptime(fecha, "%d/%m/%Y:%H")
                 date2 = datetime.strptime(allDates[i], "%d/%m/%Y:%H")
                 isDateGreater=date1 > date2
             while isDateGreater==True:
-                print(i)
+              #  print(i)
                 energia[i]=0
                 activePower[i]=0
                 voltage[i]=0
@@ -222,15 +250,16 @@ def get_data():
                 isDateGreater=date1 > date2
                 cont=i
             parsedDates=True
-            energia[cont]=((float(doc['_source']["Energia"])))
-            activePower[cont]=((float(doc['_source']["P_Activa"])))
-            voltage[cont]=((float(doc['_source']["Tension"])))
-            powerFactor[cont]=((float(doc['_source']["F_Potencia"])))
-            apparentPower[cont]=((float(doc['_source']["P_Aparente"])))
-            current[cont]=((float(doc['_source']["Intensidad"])))
-            fechas[cont]=doc['_source']["Fecha"]
+            if doc['_source'] != {}:
+                energia[cont]=((float(doc['_source']["Energia"])))
+                activePower[cont]=((float(doc['_source']["P_Activa"])))
+                voltage[cont]=((float(doc['_source']["Tension"])))
+                powerFactor[cont]=((float(doc['_source']["F_Potencia"])))
+                apparentPower[cont]=((float(doc['_source']["P_Aparente"])))
+                current[cont]=((float(doc['_source']["Intensidad"])))
+                fechas[cont]=doc['_source']["Fecha"]
             cont+=1
-        print(energia)
+      #  print(energia)
         datosEnergia= energyData(energia,activePower,voltage,powerFactor,apparentPower,current,fechas,index)
         datosEnergia = datosEnergia.toJSON()
         jsonEnergia = json.loads(datosEnergia)
@@ -371,10 +400,10 @@ def get_user_list():
 def register_user():
 
     email = request.json["email"]
-    password = request.json["password"]
     nombre=""           
     user_exists = User.query.filter_by(email=email).first() is not None
-
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(8))
     if user_exists:
         abort(409)
     hashed_password= bcrypt.generate_password_hash(password)
@@ -388,7 +417,9 @@ def register_user():
     db.session.add(new_datos) 
     db.session.add(new_user)
 
-    db.session.commit()  
+    db.session.commit() 
+    send_email(email,password)
+
     if(new_user.nombre=="nombre por defecto" or new_user.nombre==None):
         nombre= "user1"
     else:
@@ -401,11 +432,51 @@ def register_user():
         "rol": new_user.rol
     })
 
+def send_email(email,password):
+    #Crear Mensaje
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Registro EnerHome"
+    message["From"] = email_address
+    message["To"] = email
+
+    text = '''
+    Bienvenido a EnerHome 
+    Se ha registrado con el email: ''' + email +'''
+    Con la siguiente contraseña: ''' + password+'''
+    Acceda a la plataforma para cambiar la contraseña
+    '''
+
+    html = '''
+    <html>
+    <body>
+    <h1>Bienvenido a EnerHome </h1>
+    <p>Se ha registrado con el email: ''' + email+'''</p>
+    <p>Con la siguiente contraseña: ''' + password+'''</p>
+    <p>Acceda a la plataforma para cambiar la contraseña</p>
+
+    <br>
+    <a href="http://localhost:3000">EnerHome</a>
+    </body>
+    </html>
+    '''
+    texte_mime = MIMEText(text, 'plain')
+    html_mime = MIMEText(html, 'html')
+    message.attach(texte_mime)
+    message.attach(html_mime)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+  # conectarse
+        server.login(email_address, email_password)
+
+        server.sendmail(email_address, email, message.as_string())
+
 @app.route("/delete", methods=['GET','POST'])
 def delete_user():
     user_id= session["user_id"]
     User.query.filter_by(id=user_id).delete()
     db.session.commit()  
+    session.clear()
     return "Se ha eliminado el usuario correctamente"
 
 @app.route("/deleteUser", methods=['GET','POST'])
@@ -424,7 +495,16 @@ def delete_data():
     user_id= session["user_id"]
     Datos.query.filter_by(owner_id=user_id,name=filename).delete()
     db.session.commit()  
-    return "Se ha eliminado el usuario correctamente"
+    return "Se ha eliminado el fichero correctamente"
+
+@app.route("/deleteDataUser", methods=['GET','POST'])
+def delete_data2():
+    filename = request.json["filename"]
+    email= request.json["email"]
+    user = User.query.filter_by(email=email).first()
+    Datos.query.filter_by(owner_id=user.id,name=filename).delete()
+    db.session.commit()  
+    return "Se ha eliminado el fichero correctamente"
 
 @app.route("/perfil", methods=['GET','POST'])
 def update_user():
@@ -461,13 +541,9 @@ def update_user():
 def update_password():
     user = User.query.filter_by(email=session["user_email"]).first()
     password = request.json["password"]
-    lastpassword = request.json["lastPass"]
 
     if user is None:
         return jsonify({"error": "Unauthorized"}), 401
-
-    if not bcrypt.check_password_hash(user.password, lastpassword):
-        return jsonify({"error": "La contraseña actual no es correcta"}), 403
 
     if bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "La contraseña a la que se intenta cambiar es la misma"}), 400
@@ -507,10 +583,11 @@ def login_user():
         "nombre": nombre,
         "avatar": user.avatar
     })
-@app.route("/logout", methods=['GET','POST'])
+@app.route("/@logout", methods=['GET','POST'])
 def logut():
-    session.clear
-    
+    session.clear()
+    return jsonify({}), 200
+
 @app.route("/uploadfile", methods=['GET','POST'])    
 def upload_file():
 
